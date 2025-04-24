9f45918c31016bc93d2dc26d107a382a558fedf9
gitPirate12 <aneeqshaffy7@gmail.com>
2025-04-24 20:43:00 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
                   <strong>Fitfusion</strong>
                 </h2>
                 <p className="text-[rgb(179,186,198)] text-sm text-center px-4 mb-1">
-                Developed using Spring Boot for the backend and React.js with Vite for the frontend, this project helped reinforce my understanding of essential Web API concepts in Java. Since Spring Boot was completely new to me, I learned how models, repositories, and services interact with controllers in the backend. 
+                  Developed using Spring Boot for the backend and React.js with Vite for the frontend, this project helped reinforce my understanding of essential Web API concepts in Java. Since Spring Boot was completely new to me, I learned how models, repositories, and services interact with controllers in the backend.
                 </p>
                 <div className="flex gap-4 mt-1">
                   <Link
@@ -56,11 +56,40 @@ const Projects = () => {
                   <strong>ITPM-Lingo</strong>
                 </h2>
                 <p className="text-[rgb(179,186,198)] text-sm text-center px-4 mb-1">
-                Developed using Next.js, this project reinforced my understanding of creating APIs to perform CRUD operations. I also integrated free-tier APIs from RapidAPI and implemented JWT-based authentication and OAuth. Additionally, I deployed the app using Vercel and found Google Cloud Console to be a bit challenging, but it was a valuable learning experience.
+                  Developed using Next.js, this project reinforced my understanding of creating APIs to perform CRUD operations. I also integrated free-tier APIs from RapidAPI and implemented JWT-based authentication and OAuth. Additionally, I deployed the app using Vercel and found Google Cloud Console to be a bit challenging, but it was a valuable learning experience.
                 </p>
                 <div className="flex gap-4 mt-1">
                   <Link
                     href="https://github.com/gitPirate12/itpm-lingo-2.0"
+                    target="_blank"
+                  >
+                    <span className="bg-white text-[rgb(179,186,198)] px-3 py-1 rounded-md hover:text-white">
+                      <FiGithub />
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Project 3 */}
+          <div className="relative group aspect-[4/3]">
+            <div className="relative w-full h-full">
+              <Image
+                src="/project3.png"
+                alt="Flavor Exchange"
+                fill
+                className="rounded-lg shadow-lg object-cover"
+              />
+              <div className="absolute inset-0 bg-black/90 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg p-4">
+                <h2 className="text-white text-lg font-semibold mb-2 text-center">
+                  <strong>Flavor Exchange</strong>
+                </h2>
+                <p className="text-[rgb(179,186,198)] text-sm text-center px-4 mb-1">
+                  Developed using Next.js, this project reinforced my understanding of route-based APIs to implement CRUD operations. I integrated GitHub OAuth authentication using NextAuth.js for secure session management. Additionally, I deployed the application on Vercel and documented all API endpoints using Postman, enhancing my understanding of API design and documentation. This project also deepened my knowledge of Next.js Context APIs for efficient state management.
+                </p>
+                <div className="flex gap-4 mt-1">
+                  <Link
+                    href="https://github.com/gitPirate12/flavor-exchange"
                     target="_blank"
                   >
                     <span className="bg-white text-[rgb(179,186,198)] px-3 py-1 rounded-md hover:text-white">
